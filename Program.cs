af45ca52d9293a38595a06be651d7cd9fc7c5fae
Bisma Humanyun <bismahumanyun@gmail.com>
2025-01-14 21:23:45 -0500

    got issue with git write access - FinalTestQuestion12

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+namespace FinalTestQuestion12
+{
+    public class Program
+    {
+        //A method which returns a random integer between 1 to 10000 using
+        //Func. This method should return the number. Use Lamba expressions.
+        public static Func<int> GetRandomNumber = () =>
+        {
+            Random randNumber = new Random();
+            return randNumber.Next(1, 10001);
+        };
+
+        //A method which could take in a Func<int> and return a string which
+        //outputs "The Generates Number is: {number"}
+        public static string FormatGeneratedNumber(Func<int> randomNumberGenerator) {
+            int number = randomNumberGenerator();
+            return $"The Generated Number is: {number}";
+        }
+        public static void Main(string[] args)
+        {
+            Task<int> randomNumberTask = Task.Factory.StartNew(GetRandomNumber);
+            randomNumberTask.ContinueWith(task =>
+            {
+                string result = FormatGeneratedNumber(GetRandomNumber);
+                return result;
+            }).ContinueWith(task =>
+            {
+                Console.WriteLine(task.Result);
+            }).Wait();  
+   
+            //Console.WriteLine("Hello, World!");
+        }
+    }
+}
